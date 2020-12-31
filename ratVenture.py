9365d3cadb26d9c896c28f82a69d69fee70dbfbf
Zi Hui Heng <hengzihui@gmail.com>
2020-12-31 13:18:34 +0800

    Resolving Merge

--- ratVenture.py
+++ ratVenture.py
@@ -5,58 +5,17 @@ from ratVentureMenus import *
 import random
 
 main = Menu()
-choice = main.main_menu()
 
 while choice != 0:
-
-choice = 0 #global variable for main_menu choices 
-townChoice = 0 #global variable for town_menu choices 
-dayNum = 1 #global variable for the day number
-
-# UI for Town Menu
-def town_menu():
-    print("\nDay ",dayNum,": You are in a town.")
-    print("1) View Character")
-    print("2) View Map")
-    print("3) Move")
-    print("4) Rest")
-    print("5) Save Game")
-    print("6) Exit Game")
-    global townChoice
-    while 1 > townChoice or 6 < townChoice:
-        try:
-            townChoice = int(input("Enter choice: "))
-            #print(f'Choice is {townChoice}')
-        except ValueError:
-            print("\nPlease input a number between 1-6.")
-            town_menu()
-    #To implement linking with the other features
-    return townChoice
-
-# UI for Main Menu
-def main_menu():
-    print("Welcome to Ratventure!")
-    print("----------------------")
-    print("1) New Game")
-    print("2) Resume Game")
-    print("3) Exit Game")
-    global choice
-    while 1 > choice or 3 < choice:
-        try:
-            choice = int(input("Enter choice: "))
-            #print(f'Choice is {choice}')
-        except ValueError:
-            print("\nPlease input a number between 1-3.")
-            main_menu()
-
     if choice == 1:
         townChoice = main.town_menu()
-    # elif choice == 2:
-        #Enter code for resume game
-    # else:
-        #Enter code for Exit Game
+    elif choice == 2:
+        #Enter code for Resume
+        pass
+    else:
+        #Enter code for exiting game
+        pass
     choice = 0
-
 
 while townChoice != 0:
     if townChoice == 1:
@@ -107,7 +66,7 @@ while outdoorChoice != 0:
         # Enter code for exiting game
         pass
     outdoorChoice = 0
-        return choice
+
 def run():
     world = World()
     
@@ -125,6 +84,6 @@ def run():
     print(rat.id)
     damage(player,rat)
     main_menu()
+
 while True:
     run()
-main_menu()
