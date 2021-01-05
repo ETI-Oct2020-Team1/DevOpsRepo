daa21f39e58e820e17faa95e8c53b2e99cb86993
ZechariahWong <zechariahlives@gmail.com>
2021-01-05 15:53:18 +0800

    Working on the move() function. Note: Have to install pynput

--- ratVentureFunctions.py
+++ ratVentureFunctions.py
@@ -1,6 +1,22 @@
 from ratVentureObjects import *
+from pynput.keyboard import Key, Listener
 import random
 
+def on_press(key):
+    #print('{0} pressed'.format(
+        #key))
+    check_key(key)
+
+def on_release(key):
+    #print('{0} release'.format(
+       # key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+def check_key(key):
+    if key in [Key.up, Key.down, Key.left, Key.right]: 
+        print('YES')
 
 def damage(self,target):
     rawDamage = random.randint(self.attack[0],self.attack[1])
@@ -19,4 +35,14 @@ def damage(self,target):
     else:
         print(target.name, "took", calcDamage, "damage!", "\n" + target.name, "now has",target.hp, "hp left!\n")
     #GameEntity.update_entity(world,target.id,target.name,target.attack,target.defense,target.hp)
+    
+def move():
+    # Collect events until released
+    with Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
+        listener.join()
+    
+    
+    
     
