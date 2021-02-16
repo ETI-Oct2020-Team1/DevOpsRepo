a8dcb319eedb110a191d9a31069fb0caac179ffa
chriskzw <s10184964@connect.np.edu.sg>
2021-02-16 10:34:23 +0800

    updating branch

--- test_mainmenu.py
+++ test_mainmenu.py
@@ -1,18 +1,83 @@
 import pytest
-from ratVentureMenus import *
-
+from ratVenture import *
+from tud_test_base import set_keyboard_input, get_display_output
 
 world = World()
 
-def test_newgames ():
+def test_newgame ():
 
-    # main_menu(world)
-    # input = lambda: "1"
+    input_values = "1"
+    output = []
+ 
+    def mock_input(s):
+        output.append(s)
+        return input_values
+    ratVenture.input = mock_input
+    ratVenture.print = lambda s : output.append(s)
 
-    def output():
-        return "Welcome to Ratventure!\n ----------------------\n1) New Game\n2) Resume Game \n3) Exit Game \nEnter an option: \n"
+    town_menu(world)
+
+    assert output == ["View Character",\
+             "View Map",\
+             "Move",\
+             "Rest",\
+             "Save Game",\
+             "Exit Game"]
+
+def test_one ():
+
+    main_menu()
+
+    output = get_display_output()
+
+    assert output == ["New Game",\
+             "Resume Game ",\
+             "Exit Game",\
+             "Enter an option: "]
+
+def test_exitgame ():
+
+    set_keyboard_input({1})
+
+    main_menu()
+
+    output = get_display_output()
+
+    assert output == ["Day  1 : You are in a town.",\
+             "1) View Character",\
+             "2) View Map",\
+             "3) Move",\
+             "4) Rest",\
+             "5) Save Game",\
+             "6) Exit Game",\
+             "Enter an option: s"]
+
+def test_wrongchoice ():
+
+    set_keyboard_input({4})
+
+    main_menu
+
+    output = get_display_output()
+
+    assert output == ["Welcome to Ratventure!",
+    "----------------------",
+    "1)   New Game",
+    "2)   Resume Game",
+    "3)   Exit Game",
+    "Enter Choice:",
+    "\nPlease input a number between 1-3."]
 
 
-    assert output() == "Welcome to Ratventure!\n ----------------------\n1) New Game\n2) Resume Game \n3) Exit Game \nEnter an option: \n"
+
+
+
+
+#"Welcome to Ratventure!", 
+#    "----------------------", 
+#    "1)   New Game",
+#    "2)   Resume Game",
+#    "3)   Exit Game",
+#    "Enter Choice:"
 
 #"1) View Character\n2) View Map\n3) Move\n4) Rest\n5) Save Game\n 6) Exit Game\nEnter an option: "
