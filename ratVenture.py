ebd445c46058328a3f51a8edc68a8b923420961a
ZechariahWong <zechariahlives@gmail.com>
2021-02-16 18:44:46 +0800

    final commit?

--- ratVenture.py
+++ ratVenture.py
@@ -2,7 +2,7 @@ from ratVentureMenus import *
 
 
 ### Initialization
-world = World(8,8)
+world = World(3,3)
 ### Player must always be initialized first
 player = Player(world, "The Hero",[20,401],1,20)
 rat = GameEntity(world,"The Rat",[1,3],1,10)
